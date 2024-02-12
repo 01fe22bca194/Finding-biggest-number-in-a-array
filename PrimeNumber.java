c8f3ccb4cca32f0dcbf0cacba4d61335c3244568
Shraddha Jain <01fe22bca194@kletech.ac.in>
2024-02-12 19:46:13 +0530

    Code for primenumber in given range

---
+++ PrimeNumber.java
@@ -0,0 +1,40 @@
+/**
+ * The PrimeNumber class takes a range of numbers as input and prints out all the prime numbers within
+ * that range.
+ */
+import java.util.Scanner;
+
+/**
+ * The PrimeNumber class provides a method to check if a given number is prime or not.
+ */
+public class PrimeNumber {
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // The `main` method is the entry point of the Java program. It is the method that is executed when
+    // the program is run.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the range to find prime numbers (start end): ");
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+
+        System.out.println("Prime numbers between " + start + " and " + end + ":");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        scanner.close();
+    }
+}
